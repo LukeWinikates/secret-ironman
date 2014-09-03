0d21fdb178c1c22d6f43a3d0a26c94e939ad0f5c
Luke Winikates <luke.winikates@gmail.com>
2014-09-02 22:31:15 -0700

    inital setup for heroku

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject horus "0.1.0-SNAPSHOT"
+  :min-lein-version "2.0.0"
   :description "A personal dictation assistant that sends transcripts to your inbox"
   :url "http://example.com/FIXME"
   :license {:name "MIT License"
@@ -6,10 +7,12 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [ring "1.3.1"]
                  [compojure "1.1.8"]
+                 [environ "1.0.0"]
                  [speclj "2.9.0"]
                  [ring-mock "0.1.5"]]
   :plugins [[speclj "2.9.0"]]
   :test-path "spec/"
-  :main ^:skip-aot horus.core
+  :main horus.core
   :target-path "target/%s"
+  :aot [horus.core]
   :profiles {:uberjar {:aot :all}})
--- src/horus/core.clj
+++ src/horus/core.clj
@@ -1,11 +1,14 @@
 (ns horus.core
   (:use compojure.core)
-  (:require [compojure.route :as route]))
+  (:require [compojure.route :as route]
+            [compojure.handler :refer [site]]
+            [environ.core :refer [env]]
+            [ring.adapter.jetty :as jetty])
+  (:gen-class))
 
 (defroutes app
   (GET "/" [] "<h1>Horus</h1>"))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
