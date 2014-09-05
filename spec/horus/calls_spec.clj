37ab4acc1c847116ac7b776e31b1ce67ddf37401
Luke Winikates <luke.winikates@gmail.com>
2014-09-05 07:37:57 -0700

    when a user calls, record what they say

--- spec/horus/calls_spec.clj
+++ spec/horus/calls_spec.clj
@@ -5,5 +5,7 @@
 (describe "(twiml)"
   (it "tells twilio to say something"
     (should-contain "<Say>" (twiml)))
+  (it "tells twilio to record"
+    (should-contain "<Record" (twiml)))
   (it "includes the ?xml declaration"
     (should-contain "<?xml" (twiml))))
--- src/horus/calls.clj
+++ src/horus/calls.clj
@@ -1,9 +1,12 @@
 (ns horus.calls
-  (:import (com.twilio.sdk.verbs Say TwiMLResponse)))
+  (:import (com.twilio.sdk.verbs Record Say TwiMLResponse)))
 
 (defn twiml []
   (let [response (TwiMLResponse.)]
-    (.append response (Say. "Hello"))
+    (.append response (Say. "Hello, please begin your recording after the tone."))
+    (.append response
+      (doto (Record.)
+        (.setAction "/recordings")))
     (str
       "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
       (.toXML response))))
