21d8fd00899486aca2df17766ae2dd23794f829f
Luke Winikates <luke.winikates@gmail.com>
2014-09-17 13:01:24 -0700

    fix an issue where using let in speclj caused all but the last form within the let to be ignored

--- spec/horus/core_spec.clj
+++ spec/horus/core_spec.clj
@@ -1,28 +1,28 @@
 (ns horus.core-spec
   (:require [speclj.core :refer :all]
-            [horus.calls])
-  (:use horus.core
-        ring.mock.request))
+            [horus.calls]
+            [horus.core :refer [app-routes]])
+  (:use ring.mock.request))
 
 (describe "horus.core"
-  (let [app (app-routes { :sms identity })]
-    (describe "GET /"
-      (it "shows a welcome message"
-        (let [response (app (request :get "/"))
-              {status :status body :body} response]
-          (should= 200 status)
-          (should-contain "Horus" body))))
+  (with app (app-routes { :sms identity }))
+  (describe "GET /"
+    (it "shows a welcome message"
+      (let [response (@app (request :get "/"))
+            {status :status body :body} response]
+        (should= 200 status)
+        (should-contain "Horus" body))))
 
-    (describe "POST /calls"
-      (it "responds with xml"
-        (let [response (app (request :post "/calls"))
-          { status :status headers :headers body :body } response]
-          (should= "text/xml; charset=utf-8" (get headers "Content-Type"))
-          (should= body horus.calls/twiml)
-          (should= 200 status))))
+  (describe "POST /calls"
+    (it "responds with xml"
+      (let [response (@app (request :post "/calls"))
+        { status :status headers :headers body :body } response]
+        (should= "text/xml; charset=utf-8" (get headers "Content-Type"))
+        (should= body horus.calls/twiml)
+        (should= 200 status))))
 
-    (describe "POST /recordings"
-      (it "responds with 201 created"
-        (let [response (app (request :post "/recordings"))
-              { status :status} response]
-          (should= 201 status))))))
+  (describe "POST /recordings"
+    (it "responds with 201 created"
+      (let [response (@app (request :post "/recordings"))
+            { status :status} response]
+        (should= 201 status)))))
